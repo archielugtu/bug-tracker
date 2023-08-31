ce97fad54dc50a32645728f75599d2b87528920c
Rchi Lugtu <archeeeese.dev@gmail.com>
2023-08-31 13:26:47 +1200

    Added react bootstrap template

---
+++ frontend/src/components/AppHeader.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  CContainer,
+  CHeader,
+  CHeaderBrand,
+  CHeaderDivider,
+  CHeaderNav,
+  CHeaderToggler,
+  CNavLink,
+  CNavItem,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from "@coreui/icons";
+
+import { AppBreadcrumb } from "./index";
+import { AppHeaderDropdown } from "./header/index";
+import { logo } from "../assets/brand/logo";
+
+const AppHeader = () => {
+  const dispatch = useDispatch();
+  const sidebarShow = useSelector((state) => state.sidebarShow);
+
+  return (
+    <CHeader position='sticky' className='mb-4'>
+      <CContainer fluid>
+        <CHeaderToggler
+          className='ps-1'
+          onClick={() => dispatch({ type: "set", sidebarShow: !sidebarShow })}
+        >
+          <CIcon icon={cilMenu} size='lg' />
+        </CHeaderToggler>
+        <CHeaderBrand className='mx-auto d-md-none' to='/'>
+          <CIcon icon={logo} height={48} alt='Logo' />
+        </CHeaderBrand>
+        <CHeaderNav className='d-none d-md-flex me-auto'>
+          <CNavItem>
+            <CNavLink to='/' component={NavLink}>
+              Dashboard
+            </CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href='/login'>Login</CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href='/register'>Register</CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href='/404'>404</CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href='/500'>500</CNavLink>
+          </CNavItem>
+        </CHeaderNav>
+        <CHeaderNav>
+          <CNavItem>
+            <CNavLink href='#'>
+              <CIcon icon={cilBell} size='lg' />
+            </CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href='#'>
+              <CIcon icon={cilList} size='lg' />
+            </CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href='#'>
+              <CIcon icon={cilEnvelopeOpen} size='lg' />
+            </CNavLink>
+          </CNavItem>
+        </CHeaderNav>
+        <CHeaderNav className='ms-3'>
+          <AppHeaderDropdown />
+        </CHeaderNav>
+      </CContainer>
+      <CHeaderDivider />
+      <CContainer fluid>
+        <AppBreadcrumb />
+      </CContainer>
+    </CHeader>
+  );
+};
+
+export default AppHeader;
